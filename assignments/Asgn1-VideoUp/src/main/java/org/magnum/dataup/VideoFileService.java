8df6308a7c73ecc662714a21b46cd275d2b20132
Manuel Palacio <emanuel.palacio@gmail.com>
2014-08-12 17:33:35 +0200

    updated author info

--- assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/VideoFileService.java
+++ assignments/Asgn1-VideoUp/src/main/java/org/magnum/dataup/VideoFileService.java
@@ -1,6 +1,6 @@
 /*
  * 
- * Copyright 2014 Jules White
+ * Copyright 2014 Manuel Palacio
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
